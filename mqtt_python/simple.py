98d824a1594d70961a9965896da6af5d131d3fdb
introbond <introbond.dev@gmail.com>
2022-12-23 10:44:43 +0700

    done

--- mqtt_python/simple.py
+++ mqtt_python/simple.py
@@ -6,12 +6,12 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
-    print() 
+    decode = msg.payload.decode()
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("192.168.11.29", 1883, 60)
+client.connect("localhost", 1883, 60)
 
 client.loop_forever()

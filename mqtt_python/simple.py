8303dac6fd99cc712dd75d2b56e9ff66ba9c4023
introbond <introbond.dev@gmail.com>
2022-12-23 11:25:18 +0700

    add publish

--- mqtt_python/simple.py
+++ mqtt_python/simple.py
@@ -3,6 +3,7 @@ import paho.mqtt.client as mqtt
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.subscribe("JB")
+    client.publish("JB", "Hello from python")
 
 def on_message(client, userdata, msg):
     print("topic: " + msg.topic)
@@ -16,3 +17,10 @@ client.on_message = on_message
 client.connect("localhost", 1883, 60)
 
 client.loop_forever()
+
+def on_message(client, userdata, message):
+    print("Received message '" + str(message.payload) + "' on topic '" + message.topic + "'")
+
+client.on_message = on_message
+
+client.subscribe("test")

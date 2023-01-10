f378dad825e0029e709ec72c5b6004fa3b1442b2
introbond <introbond.dev@gmail.com>
2023-01-10 16:42:59 +0700

    add: coinmarketcap BTC price scraping

--- web_scraping_python/simple.py
+++ web_scraping_python/simple.py
@@ -1,9 +1,23 @@
 from bs4 import BeautifulSoup
+
 import requests
 
-url = "https://stackpython.co/courses"
-res = requests.get(url)
+target_url = "https://coinmarketcap.com/currencies/bitcoin/"
+res = requests.get(target_url)
 res.encoding = "utf-8"
 print(res)
 soup = BeautifulSoup(res.text, 'html.parser')
-print(soup.prettify())
+data_all = soup.prettify()
+find_all = soup.find_all("div",{"class":"priceValue"})
+print(find_all)
+
+btc_value = []
+
+for i in find_all:
+    i = str(i).split('<div class="priceValue"><span>', 1)
+    i = str(i).split('</span></div>', 1)
+    btc_value = i
+    break
+
+output = btc_value[0].split("['', '", 1)
+print('BTC price = ' + output[1])

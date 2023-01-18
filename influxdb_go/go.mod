f752e92cdab45c0acb318ddf099e06f0090abe31
introbond <introbond.dev@gmail.com>
2023-01-18 13:10:55 +0700

    add: struct

--- influxdb_go/go.mod
+++ influxdb_go/go.mod
@@ -4,6 +4,10 @@ go 1.19
 
 require (
 	github.com/gofiber/fiber v1.14.6 // indirect
+	github.com/gravityblast/fresh v0.0.0-20190826141211-0fa698148017 // indirect
+	github.com/howeyc/fsnotify v0.9.0 // indirect
 	github.com/influxdata/influxdb-client-go v1.4.0 // indirect
 	github.com/influxdata/influxdb-client-go/v2 v2.12.1 // indirect
+	github.com/pilu/config v0.0.0-20131214182432-3eb99e6c0b9a // indirect
+	github.com/pilu/fresh v0.0.0-20190826141211-0fa698148017 // indirect
 )
--- influxdb_go/main.go
+++ influxdb_go/main.go
@@ -2,24 +2,70 @@ package main
 
 import (
   	"fmt"
+
 	"github.com/gofiber/fiber"
-   	"github.com/influxdata/influxdb-client-go/v2"
 )
 
 func main() {
 	app := fiber.New()
-	client := influxdb2.NewClient("http://localhost:8086", "7mCH-C7vjw7ViMTI0hkDIfLm4fse5GA-2kX5BbGP-flkVtc-9sM6QKWQF9l7j7KnAMtSYtTZLlOmyZdOMWBqLQ==")
-  	writeAPI := client.WriteAPI("jb", "demo")
 
     app.Get("/", func(c *fiber.Ctx) {
         c.Send("Hello, World!")
     })
 
 	app.Post("/api/v1/devices/input", func(c *fiber.Ctx) {
-		writeAPI.WriteRecord(fmt.Sprintf("stat3,unit=temperature avg=%f", 45.0))
-		writeAPI.Flush()
+		data := new(Data)
+        if err := c.BodyParser(data); err != nil {
+            c.Status(503).Send(err)
+            return
+        }
+		fmt.Printf("data is of type %T\n", data)
+        fmt.Println(data)
+        fmt.Println(data[])
+        c.JSON(data)
     })
 
-	defer client.Close()
 	app.Listen(4000)
 }
+
+type DataInflux struct {
+	Timestamp string `json:"timestamp"`
+	Measurement string `json:"measurement"`
+	Air_port string `json:"air_port"`
+	Location string `json:"location"`
+	DevEUI string `json:"devEUI"`
+	Device string `json:"device"`
+	Rssi int `json:"rssi"`
+	Occupancy *int `json:"occupancy"`
+	Trigger_count *int `json:"trigger_count"`
+	Install *int `json:"install"`
+	Battery *float64 `json:"battery"`
+}
+
+type Data struct {
+    ApplicationName string `json:"applicationName"`
+    DeviceName string `json:"deviceName"`
+    DevEUI string `json:"devEUI"`
+    RxInfo []RxInfo `json:"rxInfo"`
+    Object *Object `json:"object"`
+    ObjectJSON *ObjectJSON `json:"objectJSON"`
+}
+
+type RxInfo struct {
+	Rssi int `json:"rssi"`
+}
+
+type Object struct {
+	Occupancy *int `json:"occupancy"`
+	Trigger_count *int `json:"trigger_count"`
+	Install *int `json:"install"`
+	Battery *float64 `json:"battery"`
+}
+
+type ObjectJSON struct {
+	Occupancy *int `json:"occupancy"`
+	Trigger_count *int `json:"trigger_count"`
+	Install *int `json:"install"`
+	Battery *float64 `json:"battery"`
+}
+

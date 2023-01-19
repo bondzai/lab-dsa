2115b46209c5c922e721fb219a9eebd7573aca3e
introbond <introbond.dev@gmail.com>
2023-01-19 13:47:51 +0700

    sent data to web-socket server

--- ws_client_go/main.go
+++ ws_client_go/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/url"
 
-	"github.com/gorilla/websocket"
+	websocket "github.com/gorilla/websocket"
 )
+
+type Data struct {
+	Message string `json:"message"`
+}
 
 func main() {
 	u := url.URL{Scheme: "ws", Host: "127.0.0.1:8000", Path: "/ws/1/"}
@@ -16,12 +21,24 @@ func main() {
 	}
 	defer c.Close()
 
-	// send message
-	// err = c.WriteMessage(websocket.TextMessage, []byte("hello, world!"))
-	// if err != nil {
-	// 	fmt.Println("write error:", err)
-	// 	return
-	// }
+	// create data to send
+	data := Data{Message: "hello from go"}
+
+	// convert data to json
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("marshal error:", err)
+		return
+	}
+
+	// send json data
+	err = c.WriteMessage(websocket.TextMessage, jsonData)
+	if err != nil {
+		fmt.Println("write error:", err)
+		return
+	}
+
+	fmt.Println("Data sent to server in json format")
 
 	// receive message
 	_, message, err := c.ReadMessage()

cf5728aa79880ae5e06adf3d65d88238acb5badd
introbond <introbond.dev@gmail.com>
2023-01-19 13:58:41 +0700

    sent data to influx

--- influxdb_go/go.mod
+++ influxdb_go/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/howeyc/fsnotify v0.9.0 // indirect
 	github.com/influxdata/influxdb-client-go v1.4.0 // indirect
 	github.com/influxdata/influxdb-client-go/v2 v2.12.1 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/pilu/config v0.0.0-20131214182432-3eb99e6c0b9a // indirect
 	github.com/pilu/fresh v0.0.0-20190826141211-0fa698148017 // indirect
 )
--- influxdb_go/main.go
+++ influxdb_go/main.go
@@ -13,38 +13,61 @@ func main() {
 	client := influxdb2.NewClient("http://localhost:8086", "7mCH-C7vjw7ViMTI0hkDIfLm4fse5GA-2kX5BbGP-flkVtc-9sM6QKWQF9l7j7KnAMtSYtTZLlOmyZdOMWBqLQ==")
 	writeAPI := client.WriteAPI("jb", "demo")
 
-	entry := []string{"Jack", "John", "Jones"}
-	for i, val := range entry {
-		fmt.Printf("At position %d, the character %s is present\n", i, val)
-	}
-
 	app.Get("/", func(c *fiber.Ctx) {
 		c.Send("JB")
 	})
 
 	app.Post("/api/v1/devices/input", func(c *fiber.Ctx) {
+
+		// get rawdata & convert from JSON to go Struct
 		data := new(Data)
 		if err := c.BodyParser(data); err != nil {
 			c.Status(503).Send(err)
 			return
 		}
-		entry := []string{"Jack", "John", "Jones"}
-		for i, val := range entry {
-			fmt.Printf("At position %d, the character %s is present\n", i, val)
-			p := influxdb2.NewPointWithMeasurement("JB").
-				AddTag("unit", "unit of measure").
-				AddField("measurement", 22.2).
-				AddField("air_port", "BKK").
-				AddField("location", 460).
-				AddField("devEUI", "ABCDEF0123").
-				AddField("device", "device-title").
-				AddField("rssi", -110).
-				SetTime(time.Now())
-			writeAPI.WritePoint(p)
-			writeAPI.Flush()
-		}
-		fmt.Println(data)
 
+		// setup data before saving into InfluxDB
+		// ========== data: static ==========
+		dataInflux := new(DataInflux)
+		dataInflux.Timestamp = time.Now()
+		dataInflux.Measurement = "1"
+		dataInflux.Air_port = "pending"
+		dataInflux.Location = "pending"
+		dataInflux.DevEUI = data.DevEUI
+		dataInflux.Device = data.DeviceName
+		dataInflux.Rssi = data.RxInfo[0].Rssi
+		// ========== data: dynamic ==========
+		// ========== data: PIR
+		dataInflux.Occupancy = data.Object.Occupancy
+		dataInflux.Trigger_count = data.Object.Trigger_count
+		// ========== data: magnetic
+
+		// monitor data for debug
+		fmt.Println(" ===== dataInflux: start =====")
+		fmt.Println("data influx: ", dataInflux)
+		fmt.Println(" ===== dataInflux: detail =====")
+		fmt.Println("data influx timestamp: ", dataInflux.Timestamp)
+		fmt.Println("data influx measurement: ", dataInflux.Measurement)
+		fmt.Println("data influx airport: (get from postgresql) ", dataInflux.Air_port)
+		fmt.Println("data influx location: (get from postgresql)", dataInflux.Location)
+		fmt.Println("data influx devEUI: ", dataInflux.DevEUI)
+		fmt.Println("data influx device: ", dataInflux.Device)
+		fmt.Println("data influx rssi: ", dataInflux.Rssi)
+		fmt.Println(" ===== dataInflux: end =====")
+
+		p := influxdb2.NewPointWithMeasurement("JB3").
+			AddTag("unit", "unit of measure").
+			AddField("measurement", 22.2).
+			AddField("air_port", dataInflux.Air_port).
+			AddField("location", dataInflux.Location).
+			AddField("devEUI", dataInflux.DevEUI).
+			AddField("device", dataInflux.Device).
+			AddField("rssi", dataInflux.Rssi).
+			SetTime(dataInflux.Timestamp)
+		writeAPI.WritePoint(p)
+		writeAPI.Flush()
+
+		fmt.Println(data)
 		c.JSON(data)
 	})
 
@@ -52,17 +75,17 @@ func main() {
 }
 
 type DataInflux struct {
-	Timestamp     string   `json:"timestamp"`
-	Measurement   string   `json:"measurement"`
-	Air_port      string   `json:"air_port"`
-	Location      string   `json:"location"`
-	DevEUI        string   `json:"devEUI"`
-	Device        string   `json:"device"`
-	Rssi          int      `json:"rssi"`
-	Occupancy     *int     `json:"occupancy"`
-	Trigger_count *int     `json:"trigger_count"`
-	Install       *int     `json:"install"`
-	Battery       *float64 `json:"battery"`
+	Timestamp     time.Time `json:"timestamp"`
+	Measurement   string    `json:"measurement"`
+	Air_port      string    `json:"air_port"`
+	Location      string    `json:"location"`
+	DevEUI        string    `json:"devEUI"`
+	Device        string    `json:"device"`
+	Rssi          int       `json:"rssi"`
+	Occupancy     *int      `json:"occupancy"`
+	Trigger_count *int      `json:"trigger_count"`
+	Install       *int      `json:"install"`
+	Battery       *float64  `json:"battery"`
 }
 
 type Data struct {

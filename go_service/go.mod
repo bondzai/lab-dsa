948db1a350319c6a5ca1d3ef034468d7feb62330
introbond <introbond.dev@gmail.com>
2023-01-22 19:47:21 +0700

    split functions

---
+++ go_service/go.mod
@@ -0,0 +1,25 @@
+module github.com/introbond/env
+
+go 1.19
+
+require (
+	github.com/andybalholm/brotli v1.0.0 // indirect
+	github.com/deepmap/oapi-codegen v1.8.2 // indirect
+	github.com/gofiber/fiber v1.14.6 // indirect
+	github.com/gofiber/utils v0.0.10 // indirect
+	github.com/gorilla/schema v1.1.0 // indirect
+	github.com/influxdata/influxdb-client-go/v2 v2.12.1 // indirect
+	github.com/influxdata/line-protocol v0.0.0-20200327222509-2487e7298839 // indirect
+	github.com/klauspost/compress v1.10.7 // indirect
+	github.com/liudng/dogo v0.0.0-20161102105440-7887b849de84 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.16.0 // indirect
+	github.com/valyala/tcplisten v0.0.0-20161114210144-ceec8f93295a // indirect
+	github.com/zhgo/config v0.0.0-20150924145423-9caf41330bd9 // indirect
+	github.com/zhgo/console v0.0.0-20150817103316-22d59bcbd28e // indirect
+	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+)
---
+++ go_service/main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gofiber/fiber"
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+)
+
+type Data struct {
+	ApplicationName string      `json:"applicationName"`
+	DeviceName      string      `json:"deviceName"`
+	DevEUI          string      `json:"devEUI"`
+	RxInfo          []RxInfo    `json:"rxInfo"`
+	Object          *Object     `json:"object"`
+	ObjectJSON      *ObjectJSON `json:"objectJSON"`
+}
+
+type RxInfo struct {
+	Rssi int `json:"rssi"`
+}
+
+type Object struct {
+	Occupancy     *int     `json:"occupancy"`
+	Trigger_count *int     `json:"trigger_count"`
+	Install       *int     `json:"install"`
+	Battery       *float64 `json:"battery"`
+}
+
+type ObjectJSON struct {
+	Occupancy     *int     `json:"occupancy"`
+	Trigger_count *int     `json:"trigger_count"`
+	Install       *int     `json:"install"`
+	Battery       *float64 `json:"battery"`
+}
+
+type DataInflux struct {
+	Timestamp     time.Time `json:"timestamp"`
+	Measurement   string    `json:"measurement"`
+	Air_port      string    `json:"air_port"`
+	Location      string    `json:"location"`
+	DevEUI        string    `json:"devEUI"`
+	Device        string    `json:"device"`
+	Rssi          int       `json:"rssi"`
+	Occupancy     *int      `json:"occupancy"`
+	Trigger_count *int      `json:"trigger_count"`
+	Install       *int      `json:"install"`
+	Battery       *float64  `json:"battery"`
+}
+
+func checkDeviceData() {
+	fmt.Println("test")
+}
+
+// func readFromPostgres() {
+
+// }
+
+func writeToInflux(dataInflux DataInflux) {
+	client := influxdb2.NewClient("http://localhost:8086", "7mCH-C7vjw7ViMTI0hkDIfLm4fse5GA-2kX5BbGP-flkVtc-9sM6QKWQF9l7j7KnAMtSYtTZLlOmyZdOMWBqLQ==")
+	writeAPI := client.WriteAPI("jb", "demo")
+
+	p := influxdb2.NewPointWithMeasurement("JB10").
+		AddTag("unit", "unit of measure").
+		AddField("measurement", 22.2).
+		AddField("air_port", dataInflux.Air_port).
+		AddField("location", dataInflux.Location).
+		AddField("devEUI", dataInflux.DevEUI).
+		AddField("device", dataInflux.Device).
+		AddField("rssi", dataInflux.Rssi).
+		SetTime(dataInflux.Timestamp)
+	writeAPI.WritePoint(p)
+	writeAPI.Flush()
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) {
+		checkDeviceData()
+		c.Send("JB")
+	})
+
+	app.Post("/api/v1/devices/input", func(c *fiber.Ctx) {
+
+		data := new(Data)
+		if err := c.BodyParser(data); err != nil {
+			c.Status(503).Send(err)
+			return
+		}
+
+		fmt.Println("==========")
+		fmt.Println("DevEUI: ", data.DevEUI)
+		fmt.Println("==========")
+
+		// ========== data: static ==========
+		dataInflux := new(DataInflux)
+		dataInflux.Timestamp = time.Now()
+		dataInflux.Measurement = "1"
+		dataInflux.Air_port = "pending"
+		dataInflux.Location = "pending"
+		dataInflux.DevEUI = data.DevEUI
+		dataInflux.Device = data.DeviceName
+		dataInflux.Rssi = data.RxInfo[0].Rssi
+		// ========== data: dynamic ==========
+		// ========== data: PIR
+		dataInflux.Occupancy = data.Object.Occupancy
+		dataInflux.Trigger_count = data.Object.Trigger_count
+		// ========== data: magnetic
+
+		// monitor data for debug
+		fmt.Println(" ===== dataInflux: start =====")
+		fmt.Println("data influx timestamp: ", dataInflux.Timestamp)
+		fmt.Println("data influx measurement: ", dataInflux.Measurement)
+		fmt.Println("data influx airport: (get from postgresql) ", dataInflux.Air_port)
+		fmt.Println("data influx location: (get from postgresql)", dataInflux.Location)
+		fmt.Println("data influx devEUI: ", dataInflux.DevEUI)
+		fmt.Println("data influx device: ", dataInflux.Device)
+		fmt.Println("data influx rssi: ", dataInflux.Rssi)
+		fmt.Println(" ===== dataInflux: end =====")
+
+		if err := writeToInflux(*dataInflux) && err != nil {}
+		c.JSON(data)
+	})
+
+	app.Listen(4000)
+}

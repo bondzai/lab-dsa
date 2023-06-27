36e5bce1406720105d69cd11723ee5dc0c674072
JamesBond <introbond.dev@gmail.com>
2023-06-27 14:05:29 +0700

    Initial commit

---
+++ amqp/rpc-go-server/go.mod
@@ -0,0 +1,5 @@
+module rpc-go
+
+go 1.20
+
+require github.com/streadway/amqp v1.1.0
---
+++ amqp/rpc-go-server/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"rpc_queue",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	err = ch.Qos(1, 0, false)
+	failOnError(err, "Failed to set QoS")
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			n := string(d.Body)
+			log.Printf("Received a request: %s", n)
+
+			// Perform the desired calculations or tasks
+			response := "Response: " + n + "1"
+
+			err = ch.Publish(
+				"",
+				d.ReplyTo,
+				false,
+				false,
+				amqp.Publishing{
+					ContentType:   "text/plain",
+					CorrelationId: d.CorrelationId,
+					Body:          []byte(response),
+				})
+			failOnError(err, "Failed to publish a response")
+
+			d.Ack(false)
+		}
+	}()
+
+	log.Printf("RPC server listening...")
+	<-forever
+}

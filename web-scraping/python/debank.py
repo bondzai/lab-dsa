01faca76bf86fe02043c3f571e59e461eedd3305
JamesBond <introbond.dev@gmail.com>
2023-07-16 12:10:42 +0700

    Update: Web browser options

--- web-scraping/python/debank.py
+++ web-scraping/python/debank.py
@@ -1,11 +1,20 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from dotenv import load_dotenv
 
 def high_level_scrape(url):
     # Configure Chrome options to run headless (without a visible browser window)
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-setuid-sandbox")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
 
     # Instantiate the Chrome driver
     driver = webdriver.Chrome(options=chrome_options)
@@ -14,26 +23,22 @@ def high_level_scrape(url):
         # Load the URL in the headless Chrome browser
         driver.get(url)
 
-        # Wait for the page to load (you can adjust the wait time based on the website's performance)
-        driver.implicitly_wait(10)  # Waits up to 10 seconds for elements to appear
+        # Wait for the specific element to load
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#Overview_defiItem__1e5s9 > div:nth-child(2)')))
 
-        # Print the raw page content (optional)
-        print(driver.page_source)
-
-        # Find the specific data using the provided selector
-        selector = "#root > div > div.DesktopFrame_mainContainer__2V8Re > div.container_mainSubContainer__39U6P > div.DesktopContainer_main__MG15v.container_pageCenterSubContainer__3encx"
-        try:
-            specific_data_element = driver.find_element_by_css_selector(selector)
-            extracted_data = specific_data_element.text
-            print("Extracted Data:")
-            print(extracted_data)
-        except NoSuchElementException:
-            print("Selector did not match any elements. Please check the selector.")
+        # Get the element's data
+        element = driver.find_element(By.CSS_SELECTOR, '#Overview_defiItem__1e5s9 > div:nth-child(2)')
+        print(element.text)  # print the content of the element
 
     finally:
-        # Close the browser window and terminate the WebDriver
         driver.quit()
 
 if __name__ == "__main__":
-    url = "https://debank.com/profile/0x1c45e086ed143aef83c1209521a2ff5369f39abc?chain=arb"
-    high_level_scrape(url)
+    load_dotenv()
+    url = os.getenv("URL")
+    url = url + "?chain=arb"
+
+    if not url:
+        print("URL not found in .env file.")
+    else:
+        high_level_scrape(url)

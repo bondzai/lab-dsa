49b585d519b496adcd325556dee45a6d90dd9e7f
JamesBond <introbond.dev@gmail.com>
2023-11-12 06:32:17 +0700

    Tidy

--- protocols/others/type-convertion/byte_string_python/simple.py
+++
@@ -1,7 +0,0 @@
-string1 = "a"
-string1_binary = ''.join(format(ord(i), '08b') for i in string1)
-string1_bytea = string1.encode()
-
-print('==========')
-print(string1_binary)
-print(string1_bytea)
--- protocols/others/typing-bot/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sudo apt-get update
-sudo apt-get install libxtst-dev libpng++-dev
